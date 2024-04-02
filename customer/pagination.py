612964a1677cefd8e484079096f662d0a72cfe2d
saugat.neupane@frostdigi.ai <saugat.neupane@frostdigi.ai>
2024-04-02 17:22:10 +0545

    fix(model):code refactor

--- customer/pagination.py
+++ customer/pagination.py
@@ -3,10 +3,11 @@ from rest_framework.response import Response
 
 
 class LargeResultsPagination(pagination.PageNumberPagination):
-    page_size = 10
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 100
 
+    #make custom response as you like
     # def get_paginated_response(self, data):
     #     return Response({
     #         'links': {
