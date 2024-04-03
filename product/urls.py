c85f534e877d712db0d025edd391d69bde7f958c
saugat.neupane@frostdigi.ai <saugat.neupane@frostdigi.ai>
2024-04-03 20:52:45 +0545

    adds url for update

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ProductCreateListView,ProductListView
-
+from .views import ProductCreateListView, ProductListView, ProductUpdateView
 
 urlpatterns = [
     path('create/', ProductCreateListView.as_view(), name='product-list-create'),
     path('list/', ProductListView.as_view(), name='product-list'),
+    path('<int:pk>/', ProductUpdateView.as_view(), name='product-update')
 ]
