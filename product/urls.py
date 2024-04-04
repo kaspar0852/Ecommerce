dc9a543a0dcc3983a55ce0146ec260b704572dd2
saugat.neupane@frostdigi.ai <saugat.neupane@frostdigi.ai>
2024-04-04 16:37:54 +0545

    adds product review urls and views

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import ProductCreateListView, ProductListView, ProductUpdateView
+from .views import (ProductCreateListView,
+                    ProductListView, ProductUpdateView,
+                    ProductReviewCreateView,ProductReviewUpdateView)
 
 urlpatterns = [
     path('create/', ProductCreateListView.as_view(), name='product-list-create'),
     path('list/', ProductListView.as_view(), name='product-list'),
-    path('<int:pk>/', ProductUpdateView.as_view(), name='product-update')
+    path('<int:pk>/', ProductUpdateView.as_view(), name='product-update'),
+    path('review/', ProductReviewCreateView.as_view(), name='product-review-create'),
+    path('review/<int:pk>/', ProductReviewUpdateView.as_view(), name='product-review-update'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics, status, filters
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer, GetProductSerializer, ProductCreateSerializer
+from .models import Product, ProductReview
+from .serializers import ProductSerializer, GetProductSerializer, ProductCreateSerializer, ProductReviewSerializer, \
+    GetProductReviewSerializer
 from django.shortcuts import get_object_or_404
 from .pagination import LargeResultsPagination
 
@@ -32,11 +33,26 @@ class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = GetProductSerializer
     pagination_class = LargeResultsPagination
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['Name', 'Description']
+    ordering_fields = ['Name']
 
     def get_queryset(self):
         queryset = super().get_queryset()
+        search_filter = self.request.query_params.get('search')
+        ordering_filter = self.request.query_params.get('ordering')
+        print(search_filter)
+
+        if search_filter:
+            queryset = queryset.filter(Name__icontains=search_filter) | queryset.filter(
+                Description__icontains=search_filter)
+
+        if ordering_filter:
+            queryset = queryset.order_by(ordering_filter)
+
         if self.request.GET.get('page'):
             page = self.paginate_queryset(queryset)
+
         return queryset
 
     def list(self, request, *args, **kwargs):
@@ -69,3 +85,40 @@ class ProductUpdateView(generics.RetrieveUpdateDestroyAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductReviewCreateView(generics.ListCreateAPIView):
+    queryset = ProductReview.objects.all()
+    pagination_class = LargeResultsPagination
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['Rating']
+    ordering_fields = ['Rating']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_filter = self.request.query_params.get('search')
+        ordering_filter = self.request.query_params.get('ordering')
+
+        if search_filter:
+            queryset = queryset.filter(Rating__icontains=search_filter)
+
+        if ordering_filter:
+            queryset = queryset.order_by(ordering_filter)
+
+        if self.request.GET.get('page'):
+            self.paginate_queryset(queryset)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return GetProductReviewSerializer
+        return ProductReviewSerializer
+
+
+class ProductReviewUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductReview.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return GetProductReviewSerializer
+        return ProductReviewSerializer
