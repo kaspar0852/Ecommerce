b7057010baf37366e6dfeb74538579bb2253f36c
saugat.neupane@frostdigi.ai <saugat.neupane@frostdigi.ai>
2024-04-04 16:37:37 +0545

    adds django filters to settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',
+    'django_filters',
     'drf_yasg',
     'customer',
     'product'
@@ -130,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
