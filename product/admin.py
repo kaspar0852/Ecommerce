c29d76a092668f823ee9a82e14579ad15962eb3f
saugat.neupane@frostdigi.ai <saugat.neupane@frostdigi.ai>
2024-04-04 16:37:00 +0545

    adds product review model

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from product.models import Product,ProductType,ProductImage
+from product.models import Product,ProductType,ProductImage,ProductReview
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(ProductType)
 admin.site.register(ProductImage)
+admin.site.register(ProductReview)
--- product/models.py
+++ product/models.py
@@ -9,7 +9,7 @@ from customer.models import User
 
 class ProductType(models.Model):
     Name = models.CharField(max_length=250)
-    Description = models.TextField(max_length=250,null=True, blank=True)
+    Description = models.TextField(max_length=250, null=True, blank=True)
 
     def __str__(self):
         return self.Name
@@ -30,8 +30,20 @@ class ProductImage(models.Model):
     Name = models.CharField(max_length=250)
     Image = models.BinaryField()
     Extension = models.CharField(max_length=20)
-    ProductId = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='image')
-    #here a related name is defined so i can use this in the serializer to show relation with product
+    ProductId = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='image')
+
+    # here a related name is defined so i can use this in the serializer to show relation with product
 
     def __str__(self):
         return self.Name
+
+
+class ProductReview(models.Model):
+    ProductId = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
+    UserId = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    Rating = models.IntegerField()
+    Review_Text = models.TextField()
+    Review_Date = models.DateField()
+
+    def __str__(self):
+        return self.Rating
